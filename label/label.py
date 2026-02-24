a1a6761310c14b45200876ae3b8938a52b43b717
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-02-24 19:45:57 +0900

    Update label.py

--- label/label.py
+++ label/label.py
@@ -20,7 +20,7 @@ for j in range(len(input)):
         hamidashi = True
         break
     elif i == 4:
-        if num < 20:
+        if num <= 20:
             if input[j] == "\n":
                 texts[i] += "  \n"
                 i += 1
