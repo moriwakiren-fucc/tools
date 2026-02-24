62264ef90262162b827abccdad23eabc1cb8852f
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-02-24 09:24:32 +0900

    Update label.py

--- label/label.py
+++ label/label.py
@@ -1,3 +1,14 @@
 import streamlit as st
 st.title('メッセージを入力！')
-text = st.text_area(None)
+input = st.text_area("")
+text = ""
+num
+i = 0
+texts = []
+for j in range(len(input)):
+    if num > 34:
+        texts[i] = text + input[j]
+        num += 1
+    else:
+        i += 1
+st.write(texts)
