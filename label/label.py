eb499a9204357ff9cec65f81dd7f62d7579af8f1
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-02-24 09:51:41 +0900

    Update label.py

--- label/label.py
+++ label/label.py
@@ -9,7 +9,7 @@ texts = [""]
 for j in range(len(input)):
     if i > 4:
         hamidashi = True
-    if num < 34:
+    elif num < 34:
         if input[j] == "\n":
             texts[i] += "  \n"
             i += 1
