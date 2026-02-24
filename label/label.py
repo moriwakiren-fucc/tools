a97ddfc04c3239dd2b5ff1c3800ff91e489cfc71
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-02-24 09:42:14 +0900

    Update label.py

--- label/label.py
+++ label/label.py
@@ -5,16 +5,17 @@ text = ""
 num = 0
 i = 0
 texts = [""]
-for j in range(len(input)):
-    if num < 34:
-        if input[j] == "\n":
-            texts[i] += "  \n"
+while num <= 4:
+    for j in range(len(input)):
+        if num < 34:
+            if input[j] == "\n":
+                texts[i] += "  \n"
+            else:
+                texts[i] += input[j]
+            num += 1
         else:
-            texts[i] += input[j]
-        num += 1
-    else:
-        num = 0
-        texts.append("")
-        i += 1
+            num = 0
+            texts.append("")
+            i += 1
 output = "  \n".join(texts)
 st.write(output)
