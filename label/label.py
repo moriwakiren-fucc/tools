81c0b293022d0d280052f2ac0fbbf62894c684c3
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-02-24 13:32:38 +0900

    Update label.py

--- label/label.py
+++ label/label.py
@@ -64,5 +64,5 @@ st.header("コピー（送信用）")
 text_to_copy = "\n".join(texts) + '\n｜' + name + "\nhttps://ujwp3bjnyphoklyj4gntpc.streamlit.app"
 
 # HTMLとJavaScriptを使用してクリップボードにコピーする機能を実装
-copy_html = f"""<button onclick='navigator.clipboard.writeText(\"{text_to_copy}\")'>Copy to clipboard<\/button>"""
+copy_html = f"""<button onclick='navigator.clipboard.writeText(\"{text_to_copy}\")'>Copy to clipboard</button>"""
 html(copy_html, height=50)
