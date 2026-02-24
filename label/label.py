741849d3b1fcc33015680bc47cc9dc62cf2c407d
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-02-24 13:42:06 +0900

    Update label.py

--- label/label.py
+++ label/label.py
@@ -59,7 +59,7 @@ st.divider()
 st.header("コピー（送信用）")
 
 # コピーするテキスト
-text = "\n".join(texts) + '\n｜' + name + "\nhttps://ujwp3bjnyphoklyj4gntpc.streamlit.app"
+text = r"\n".join(texts) + r"\n｜" + name + r"\nhttps://ujwp3bjnyphoklyj4gntpc.streamlit.app"
 
 st.code(text, language="")
 
