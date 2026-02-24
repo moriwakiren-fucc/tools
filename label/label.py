9171291e5857ece87f4d8a37a485ad56bcb3ac04
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-02-24 19:49:34 +0900

    Update label.py

--- label/label.py
+++ label/label.py
@@ -2,7 +2,6 @@ import re
 import streamlit as st
 import time
 st.set_page_config(page_title="ラベル（メッセージ）入力")
-st.header('入力')
 name = st.text_input("名前　 ※全角「 25/7 文字」まで（半角は 4/7 文字扱い）")
 input = st.text_area("メッセージ 　※1行あたり全角「 25 文字」まで（半角は 4/7 文字扱い）")
 if st.button("文字数判定＆プレビュー反映"):
